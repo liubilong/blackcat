5df8447f0aa9929876574d4ea3a49ba9e32d76a2
BilongLiu <liubilong@gsafety.com>
2020-04-23 10:43:02 +0800

    test02

--- src/main/java/com/sileton/blackcat/BlackCatApp.java
+++ src/main/java/com/sileton/blackcat/BlackCatApp.java
@@ -1,5 +1,6 @@
 package com.sileton.blackcat;
 
+import org.apache.commons.lang3.CharUtils;
 
 import com.sileton.blackcat.utils.CommandLineInput;
 import com.sileton.blackcat.utils.CommandLineInputHandler;
@@ -14,8 +15,9 @@ public class BlackCatApp {
         while (CommandLineInput.EXIT.getShortCmd() != command) {
             commandLineInputHandler.printOptions();
             String input = commandLineInputHandler.readInput();
-            char[] inputChars = input.length() == 1 ? input.toCharArray() : new char[]{DEFAULT_INPUT};
-            command = inputChars[0];
+            //char[] inputChars = input.length() == 1 ? input.toCharArray() : new char[]{DEFAULT_INPUT};
+            //command = inputChars[0];
+            command = CharUtils.toChar(input,DEFAULT_INPUT);
             CommandLineInput commandLineInput = CommandLineInput.getCommandLineInputForInput(command);
             commandLineInputHandler.processInput(commandLineInput);
         }
